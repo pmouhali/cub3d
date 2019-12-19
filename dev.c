bd18f7d35e194fd29a86a92a06df453110b5b3e8
Paloma MOUHALI <pmouhali@e1r8p10.42.fr>
2019-12-19 16:26:22 +0100

    keyhook working, we can draw a continuous line on the screen, no key repeat

--- dev.c
+++ dev.c
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <mlx.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 typedef struct	s_rgba
 {
@@ -16,6 +18,18 @@ typedef struct	s_line
 	int y; // on screen height, y position of the firt point
 	int z; // on screen height, y position of the second point
 }		t_line;
+
+typedef struct	s_parameters
+{
+	int keycode;
+	int posx;
+	int posy;
+	void *mlx;
+	void *win;
+	void *img;
+	void *img_tab;
+	t_rgba color;
+}		t_parameters; // current position and keycode hitted
 
 void	mlx_img_draw_vertical_line(void **img, t_line line, t_rgba color)
 {
@@ -35,6 +49,44 @@ void	mlx_img_draw_vertical_line(void **img, t_line line, t_rgba color)
 
 		i++;
 	}
+}
+
+void	mlx_img_draw_pixel(void **img, int x, int y, t_rgba color)
+{
+	int pp;
+	unsigned char *c;
+
+	c = *img;
+	pp = x * 4 + 4 + 3200 * y;
+	c[pp] = color.r;	
+	c[pp + 1] = color.g;	
+	c[pp + 2] = color.b;	
+}
+
+int	key_hook(int keycode,void *params)
+{
+	t_parameters *copy;
+
+	copy = params;
+	copy->keycode = keycode;
+	if (keycode == 123)
+		copy->posx -= 1;
+	if (keycode == 124)
+		copy->posx += 1;
+	if (keycode == 125)
+		copy->posy += 1;
+	if (keycode == 126)
+		copy->posy -= 1;
+
+	char *t;
+	t = copy->img_tab;
+
+	mlx_img_draw_pixel((void**)&t, copy->posx, copy->posy, copy->color);
+	mlx_put_image_to_window(copy->mlx, copy->win, copy->img, 0, 0);
+	
+	if (copy->keycode == 53)
+		exit(1);
+	return (0);
 }
 
 int	main(void)
@@ -64,19 +116,28 @@ int	main(void)
 	t_line line;
 	t_rgba color;
 
-	line.x = 400;
+	line.x = 200;
 	line.y = 0;
-	line.z = 600;
+	line.z = 400;
 
 	color.r = 0;
 	color.g = 255;
 	color.b = 0;
 	color.a = 0;
 
-	mlx_img_draw_vertical_line(&test, line, color);
+	t_parameters params;
 	
-	mlx_put_image_to_window(id, window_id, img, 0, 0);
+	params.posx = 400;
+	params.posy = 400;
 
+	params.mlx = id;
+	params.win = window_id;
+	params.img = img;
+	params.img_tab = test;
+	params.color = color;
+
+	mlx_key_hook(window_id, &key_hook, &params);
+		
 	mlx_loop(id);
 	return (0);
 }
