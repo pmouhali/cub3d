5277d9a9e8b2514263d9efec3ca0cdff8bb94337
Paloma MOUHALI <pmouhali@e2r8p1.42.fr>
2020-01-21 18:26:36 +0100

    norme: srcs/utils

--- srcs/utils/buffer_to_image.c
+++ srcs/utils/buffer_to_image.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   buffer_to_image.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/21 18:21:53 by pmouhali          #+#    #+#             */
+/*   Updated: 2020/01/21 18:23:09 by pmouhali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "header.h"
 
 void	buffer_to_image(int h, int w, int **buf, void **img)
 {
-	unsigned char *c;
-	int i;
-	int a;
-	int b;
+	unsigned char	*c;
+	int				i;
+	int				a;
+	int				b;
 
 	c = *img;
 	a = 0;
@@ -15,8 +27,8 @@ void	buffer_to_image(int h, int w, int **buf, void **img)
 		b = 0;
 		while (b < w)
 		{
-			c[i + 2] = buf[a][b] / 65536;	
-			c[i + 1] = (buf[a][b] / 256) % 256;	
+			c[i + 2] = buf[a][b] / 65536;
+			c[i + 1] = (buf[a][b] / 256) % 256;
 			c[i] = buf[a][b] % 256;
 			b++;
 			i += 4;
--- srcs/utils/draw_scene.c
+++ srcs/utils/draw_scene.c
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_scene.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/21 18:22:00 by pmouhali          #+#    #+#             */
+/*   Updated: 2020/01/21 18:26:27 by pmouhali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "header.h"
-#include <stdio.h>
 
 int	**draw_scene(t_parameters p)
 {
-	t_dda_parameters dda;
-	int **buffer;
-	int x = 0;
-	double *zbuffer;
+	t_dda_parameters	dda;
+	int					**buffer;
+	int					x;
+	double				*zbuffer;
 
 	if (!(buffer = (int**)new_tda(sizeof(int), p.win_h, p.win_w)))
 		return (NULL);
 	if (!(zbuffer = (double*)malloc(sizeof(double) * p.win_w)))
 		return (NULL);
+	x = 0;
 	while (x < p.win_w)
 	{
 		dda_init(p, &dda, x);
