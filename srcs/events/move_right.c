c3eda9f73a9d148103ca17b2553cf434e5c45e94
Paloma MOUHALI <pmouhali@e2r8p1.42.fr>
2020-01-21 18:16:55 +0100

    norme: srcs/events

--- srcs/events/move_right.c
+++ srcs/events/move_right.c
@@ -1,10 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_right.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/21 18:14:03 by pmouhali          #+#    #+#             */
+/*   Updated: 2020/01/21 18:16:36 by pmouhali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "header.h"
 
 void	move_right(t_parameters *p)
 {
-	double ndirx = p->diry;
-	double ndiry = -p->dirx;	
+	double ndirx;
+	double ndiry;
 
+	ndirx = p->diry;
+	ndiry = -p->dirx;
 	if (p->map[(int)(p->posx + ndirx * MOVESPEED)][(int)p->posy] == '0')
 		p->posx += ndirx * MOVESPEED;
 	if (p->map[(int)p->posx][(int)(p->posy + ndiry * MOVESPEED)] == '0')
