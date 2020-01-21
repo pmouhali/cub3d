8e4577da5ba60db1d9a00358920bb725c50ceb4b
Paloma MOUHALI <pmouhali@e2r8p1.42.fr>
2020-01-21 18:10:55 +0100

    norme: header

--- main.c
+++ main.c
@@ -1,45 +1,22 @@
-#include "header.h"
-#include <stdio.h> 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/21 15:47:43 by pmouhali          #+#    #+#             */
+/*   Updated: 2020/01/21 15:47:54 by pmouhali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-static void	print_params(t_parameters p)
-{
-	printf("resolution: %d x %d\n", p.win_w, p.win_h);
-	printf("map: %d x %d\n", p.map_w, p.map_h);
-	int i = -1;
-	while (++i < p.map_h && p.map && (p.map)[i])
-		printf("%s\n", (p.map)[i]);
-	printf("\nstart position: x%f y%f\n", p.posx, p.posy);
-	printf("direction vector: x%f y%f\n", p.dirx, p.diry);
-	printf("camera plane: x%f y%f\n\n", p.planex, p.planey);
-	printf("mlx_init(): %p\n", p.mlx_id);
-	printf("new_window(): %p\n", p.win_id);
-	printf("new_image(): %p\n", p.img_id);
-	printf("image bits per pixel: %d\n", p.bpp);
-	printf("north_texture id: %p\n", p.north_texture.id);
-	printf("north_texture height: %d\n", p.north_texture.height);
-	printf("north_texture width: %d\n", p.north_texture.width);
-	printf("south_texture id: %p\n", p.south_texture.id);
-	printf("south_texture height: %d\n", p.south_texture.height);
-	printf("south_texture width: %d\n", p.south_texture.width);
-	printf("west_texture id: %p\n", p.west_texture.id);
-	printf("west_texture height: %d\n", p.west_texture.height);
-	printf("west_texture width: %d\n", p.west_texture.width);
-	printf("east_texture id: %p\n", p.east_texture.id);
-	printf("east_texture height: %d\n", p.east_texture.height);
-	printf("east_texture width: %d\n", p.east_texture.width);
-	printf("sprite_texture id: %p\n", p.sprite_texture.id);
-	printf("sprite_texture height: %d\n", p.sprite_texture.height);
-	printf("sprite_texture width: %d\n", p.sprite_texture.width);
-	printf("floor color: %d\n", p.floor_color);
-	printf("ceiling color: %d\n", p.ceiling_color);
-}
+#include "header.h"
 
 int	main(int ac, char **av)
 {
 	t_parameters params;
 
 	initialize_cub3d(&params, av[1]);
-	print_params(params);
 	if (ac > 2 && ft_strcmp(av[2], "-save") == 0)
 	{
 		save_scene(&params);
@@ -47,8 +24,8 @@ int	main(int ac, char **av)
 	}
 	initialize_mlx_window(&params);
 	display_scene(params);
-	mlx_hook(params.win_id, 2, 0, &key_hook, &params);		
-	mlx_hook(params.win_id, 17, 0, &close_button, &params);		
+	mlx_hook(params.win_id, 2, 0, &key_hook, &params);
+	mlx_hook(params.win_id, 17, 0, &close_button, &params);
 	mlx_loop(params.mlx_id);
 	quit_program(&params, NULL);
 }
--- srcs/header.h
+++ srcs/header.h
@@ -1,164 +1,180 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmouhali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/21 15:48:56 by pmouhali          #+#    #+#             */
+/*   Updated: 2020/01/21 18:10:19 by pmouhali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HEADER_H
 # define HEADER_H
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-#include "../libmlx/mlx.h"
-#include "../libft/libft.h"
-#include "../libbitmapfile/bitmapfile.h"
+# include "../libmlx/mlx.h"
+# include "../libft/libft.h"
+# include "../libbitmapfile/bitmapfile.h"
 
 typedef struct	s_texture
 {
 	void	*id;
-	int	*img;
+	int		*img;
 	int		width;
 	int		height;
-}		t_texture;
+}				t_texture;
 
-typedef struct s_sprite
+typedef struct	s_sprite
 {
-	double x;
-	double y;
-	double distance;
-}		t_sprite;
+	double	x;
+	double	y;
+	double	distance;
+}				t_sprite;
 
 typedef struct	s_sprites_parameters
 {
-	double invdet;
-	double transformx;
-	double transformy;
-	int screenx;
-	int h;
-	int w;
-	int dstart_x;
-	int dstart_y;
-	int dend_x;
-	int dend_y;
-	int tex_x;
-}		t_sprites_parameters;
+	double	invdet;
+	double	transformx;
+	double	transformy;
+	int		screenx;
+	int		h;
+	int		w;
+	int		dstart_x;
+	int		dstart_y;
+	int		dend_x;
+	int		dend_y;
+	int		tex_x;
+}				t_sprites_parameters;
 
 typedef struct	s_parameters
 {
-	char *config_file;
-	int win_h;
-	int win_w;
-	int map_h;
-	int map_w;
-	char **map;
-
-	int nsprite;
-	t_sprite *sprites;	
-
-	double posx;
-	double posy;
-	double dirx;
-	double diry;
-	double planex;
-	double planey;
-
-	void *mlx_id;
-	void *win_id;
-	void *img_id;
-	int endian;
-	int bpp;
-	int line_size;
-	void *img;
-
+	char		*config_file;
+	int			win_h;
+	int			win_w;
+	int			map_h;
+	int			map_w;
+	char		**map;
+	int			nsprite;
+	t_sprite	*sprites;
+	double		posx;
+	double		posy;
+	double		dirx;
+	double		diry;
+	double		planex;
+	double		planey;
+	void		*mlx_id;
+	void		*win_id;
+	void		*img_id;
+	int			endian;
+	int			bpp;
+	int			line_size;
+	void		*img;
 	t_texture	north_texture;
 	t_texture	south_texture;
 	t_texture	west_texture;
 	t_texture	east_texture;
 	t_texture	sprite_texture;
-
-	int floor_color;
-	int ceiling_color;
-
-}		t_parameters;
+	int			floor_color;
+	int			ceiling_color;
+}				t_parameters;
 
 typedef struct	s_dda_parameters
 {
-	double camerax;
-	double raydirx;
-	double raydiry;
-	int mapx;
-	int mapy;
-	double sidedistx;
-	double sidedisty;
-	double deltadistx;
-	double deltadisty;
-	double perpwalldist;
-	int stepx;
-	int stepy;
-	int hit;
-	int side;
-	int lineheight;
-	int linex;
-	int liney;
-	double wallx;
+	double	camerax;
+	double	raydirx;
+	double	raydiry;
+	int		mapx;
+	int		mapy;
+	double	sidedistx;
+	double	sidedisty;
+	double	deltadistx;
+	double	deltadisty;
+	double	perpwalldist;
+	int		stepx;
+	int		stepy;
+	int		hit;
+	int		side;
+	int		lineheight;
+	int		linex;
+	int		liney;
+	double	wallx;
 
-}		t_dda_parameters;
+}				t_dda_parameters;
 
-#define MOVESPEED 0.16
-#define ROTSPEED 0.09
+# define MOVESPEED 0.16
+# define ROTSPEED 0.09
 
-#define MAX_WINDOW_WIDTH 2560
-#define MAX_WINDOW_HEIGHT 1440
+# define MAX_WINDOW_WIDTH 2560
+# define MAX_WINDOW_HEIGHT 1440
 
-#define NORTH 3
-#define SOUTH 4
-#define EAST 5
-#define WEST 6
+# define NORTH 3
+# define SOUTH 4
+# define EAST 5
+# define WEST 6
 
-#define DK_LEFT 123
-#define DK_RIGHT 124
-#define K_W 13 
-#define K_A 0
-#define K_S 1
-#define K_D 2
-#define K_ESC 53
+# define DK_LEFT 123
+# define DK_RIGHT 124
+# define K_W 13
+# define K_A 0
+# define K_S 1
+# define K_D 2
+# define K_ESC 53
 
-int     close_button(t_parameters *p);
-void	camera_left(t_parameters *p);
-void	camera_right(t_parameters *p);
-void    move_forward(t_parameters *p);
-void    move_backward(t_parameters *p);
-void    move_left(t_parameters *p);
-void    move_right(t_parameters *p);
-void	mlx_clear_img(void **img);
-void    initialize_mlx_window(t_parameters *p);
-void    initialize_cub3d(t_parameters *params, const char *arg1);
-void	sort_sprites(t_sprite *sprites, int array_size);
-void    set_sprites(t_parameters *p);
-void    sprites_distance(t_sprite *sprites, int size, int posx, int posy);
-void    draw_sprites(int **buf, t_parameters p, double *zbuffer);
-int		**draw_scene(t_parameters tmp);
-void    save_scene(t_parameters *params);
-void    display_scene(t_parameters params);
-void    dda_init(t_parameters params, t_dda_parameters *dda, int screen_slice);
-void    dda_perform(t_dda_parameters *dda, char **map);
-void    dda_data(t_dda_parameters *dda, t_parameters p, double *z);
-void    draw_textured_stripe(int **buf, int s, t_dda_parameters dda, t_parameters p);
-int     key_hook(int keycode,void *params);
-void    buffer_to_image(int h, int w, int **buf, void **img);
-void	quit_program(t_parameters *params, const char *error_msg);
-void	init_params(t_parameters *params, const char *filepath);
-void	set_params_to_default(t_parameters *params);
-int		validate_params(t_parameters *params);
-int    validate_file(t_parameters *params, const char *filepath);
+void			save_scene(t_parameters *params);
+void			display_scene(t_parameters params);
+void			quit_program(t_parameters *params, const char *error_msg);
 
-void	set_params(t_parameters *params, const char *line);
-void	set_resolution(t_parameters *params, const char *line);
-void	set_texture(t_parameters *params, t_texture *t, const char *line);
-void	set_floor_color(t_parameters *params, const char *line);
-void	set_ceiling_color(t_parameters *params, const char *line);
-void	set_map(t_parameters *params, char **line, int l);
-int			validate_map(t_parameters *params);
-void    set_player_position(t_parameters *params, int c, int x, int y);
-int		validate_line_type1(char *map_line, int len);
-int		validate_line_type2(char *map_line, int l, t_parameters *params);
+void			set_params_to_default(t_parameters *params);
+void			init_params(t_parameters *params, const char *filepath);
+int				validate_params(t_parameters *params);
+
+void			initialize_mlx_window(t_parameters *p);
+void			initialize_cub3d(t_parameters *params, const char *arg1);
+
+int				validate_file(t_parameters *params, const char *filepath);
+void			set_params(t_parameters *params, const char *line);
+void			set_resolution(t_parameters *params, const char *line);
+void			set_texture(
+				t_parameters *params, t_texture *t, const char *line);
+void			set_floor_color(t_parameters *params, const char *line);
+void			set_ceiling_color(t_parameters *params, const char *line);
+void			set_map(t_parameters *params, char **line, int l);
+int				validate_map(t_parameters *params);
+void			set_player_position(t_parameters *params, int c, int x, int y);
+int				validate_line_type1(char *map_line, int len);
+int				validate_line_type2(
+					char *map_line, int l, t_parameters *params);
+void			set_sprites(t_parameters *p);
+
+void			dda_init(
+				t_parameters params, t_dda_parameters *dda, int screen_slice);
+void			dda_perform(t_dda_parameters *dda, char **map);
+void			dda_data(t_dda_parameters *dda, t_parameters p, double *z);
+void			draw_textured_stripe(
+				int **buf, int s, t_dda_parameters dda, t_parameters p);
+void			sort_sprites(t_sprite *sprites, int array_size);
+void			sprites_distance(
+				t_sprite *sprites, int size, int posx, int posy);
+void			draw_sprites(int **buf, t_parameters p, double *zbuffer);
+
+void			mlx_clear_img(void **img);
+int				**draw_scene(t_parameters tmp);
+void			buffer_to_image(int h, int w, int **buf, void **img);
+
+int				key_hook(int keycode, void *params);
+int				close_button(t_parameters *p);
+void			camera_left(t_parameters *p);
+void			camera_right(t_parameters *p);
+void			move_forward(t_parameters *p);
+void			move_backward(t_parameters *p);
+void			move_left(t_parameters *p);
+void			move_right(t_parameters *p);
 
 #endif
